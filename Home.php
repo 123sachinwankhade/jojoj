d3c2126dba6b12c0ae897e35a28ae4c80ba5bc14
123sachinwankhade <sachinwankhade1998@gmail.com>
2019-11-03 22:18:07 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home page</title>
+<style>
+p{
+	font-size:20px;
+}
+    a
+    {
+        text-decoration:none;
+    }
+</style>
+</head>
+<body background="http://localhost/MTest/images/e.jpg"">
+    
+    <p style="color:black">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspThe vision of the Heramb Coaching Classes is to be a cross-faculty unit that contributes to continual improvement in<br> the quality of higher education by <b>Professor Vishwanathan Iyer</b> through widening access,promoting excellence through equity,<br> developing the curriculum in partnership with faculties,enhancing the competence of graduates by ensuring the<br> provision of keys skills.We conduct Coaching Classes for <b>XI,XII,B.COM and M.COM</b>.The Department has seven full time<br> faculty representing expertise in a vriety of curriculum fields.We also have our classes for C.A. P.E.I& P.E.II.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp<b>Heramb Coaching Classes</b> was inaugurated on 25th october,2002 with a handful of students.Such was the impact<br> of the results of that year in the minds of people of kalyan(East) that the number of students grew manifold in the next<br> academic year.The year 2003-04 was really a feather in the cap of this oraganization with the best results produced at both<br>levels-XII commerce and T.Y.B.COM.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspUnderstanding the requirement of an average student in Kalyan(East),Heramb <b>Professional Institue</b> was inaugurated<br> for students perparing themselves for professional courses.
+</p>
+    <h3><a href="index.php">Back to Home</a></h3>
+</body>
+</html>
